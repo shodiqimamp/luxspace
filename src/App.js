b298278cb3e66421f80432d2a5fe37efddaa654d
shodiqimamp <shodiqimamp@gmail.com>
2022-06-20 12:26:04 +0700

    commit 1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './assets/css/app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-white bg-black text-center px-2 py-4 text-ellipsis"> Nama Saya Shodiq Imam Purnomo </h1>
+    </div>
+  );
+}
+
+export default App;
