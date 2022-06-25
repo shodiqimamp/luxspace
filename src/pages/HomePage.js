135c7f7cfbefeab220ad72316374ae423a704f46
shodiqimamp <shodiqimamp@gmail.com>
2022-06-25 17:48:27 +0700

    add hook scrollanchor and modaldom

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
-import Header from '../parts/Header';
-import Hero from '../parts/homepage/Hero';
-import JustArr from '../parts/homepage/JustArrived';
-import BrowseRoom from '../parts/homepage/BrowseRoom';
-import Client from '../parts/Client';
-import Sitemap from '../parts/Sitemap';
-import Footer from '../parts/Footer';
-export default function homepage() {
+import Header from "../parts/Header";
+import Hero from "../parts/homepage/Hero";
+import JustArr from "../parts/homepage/JustArrived";
+import BrowseRoom from "../parts/homepage/BrowseRoom";
+import Client from "../parts/Client";
+import Sitemap from "../parts/Sitemap";
+import Footer from "../parts/Footer";
+
+import useScrollAnchor from "../helpers/hooks/useScrollAnchor";
+import useModalDOM from "../helpers/hooks/useModalDOM";
+
+export default function Homepage() {
+  useScrollAnchor();
+  useModalDOM();
+
   return (
     <>
        <Header theme={"white"} position={"absolute"}/>
